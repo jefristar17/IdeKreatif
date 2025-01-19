2a34343b1cabc0874166f447c15a5955a62e0517
jefrial-rpl <jeff711720@gmail.com>
2025-01-19 19:50:54 +0700

    menambahkan register process

--- auth/register_process.php
+++ auth/register_process.php
@@ -3,7 +3,7 @@ require_once("../config.php");
 // mulai session
 session_start();
 
-if($_SERVERr["REQUEST_METHOD"] == "POST") {
+if($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST["username"];
     $name = $_POST["name"];
     $password = $_POST["password"];
