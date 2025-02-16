6d4eb483a190e601c57f10f0d8aa5cdfe7c6fcd2
jefrial-rpl <jeff711720@gmail.com>
2025-02-16 20:19:59 +0700

    menambahkan login auth

--- auth/login_auth.php
+++ auth/login_auth.php
@@ -40,7 +40,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             ];
         }
 
-        header('Location: ../login.php');
+        header('Location: login.php');
         exit();
     }
     $conn->close();
