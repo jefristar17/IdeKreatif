ced999a9ef6ffee348b1396bfa5dd4c860f5e6f8
jefrial-rpl <jeff711720@gmail.com>
2025-02-16 20:00:28 +0700

    menambahkan dashboard

--- dashboard.php
+++ dashboard.php
@@ -1,111 +1,102 @@
 <?php
 include (".includes/header.php");
-$title = "dashboard";
-// menyertakan file untuk menampilkan notifikasi (jika ada)
+$title = "Dashboard";
+// Menyertakan file untuk menampilkan notifikasi (jika ada)
 include '.includes/toast_notification.php';
 ?>
+
 <div class="container-xxl flex-grow-1 container-p-y">
-  <!-- card untuk menampilkan tabel postingan -->
-  <div class="card">
-    <!-- tabel dengan baris yang dapat di-hover-->
+    <!-- Card untuk menampilkan tabel postingan -->
     <div class="card">
-      <!-- header tabel -->
-      <div class="card-header d-flex justify-content-between align-items-center">
-        <h4>Semua Postingan</h4>
-      </div>
-      <div class="card-body">
-      <!-- tabel responsif -->
-       <div class="table-responsive text-nowrap">
-        <table id="datatable" class="table table-hover">
-            <thead>
-                <tr class="text-center">
-                    <th width="50px">#</th>
-                    <th>Judul post</th>
-                    <th>Penulis</th>
-                    <th>Kategori</th>
-                </tr>
-            </thead>
-            <tbody class="tables-border-bottom-0">
+        <!-- Tabel dengan baris yang dapat di-hover -->
+        <div class="card">
+            <!-- Header Tabel -->
+            <div class="card-header d-flex justify-content-between align-items-center">
+                <h4>Semua Postingan</h4>
+            </div>
+            <div class="card-body">
+                <!-- Tabel responsif -->
+                <div class="table-responsive text-nowrap">
+                    <table id="datatable" class="table table-hover">
+                        <thead>
+                            <tr class="text-center">
+                                <th width="50px">#</th>
+                                <th>Judul Post</th>
+                                <th>Penulis</th>
+                                <th>Kategori</th>
+                                <th width="150px">Pilihan</th>
+                            </tr>
+                        </thead>
+                        <tbody class="table-border-bottom-0">
+                            <!-- Menampilkan data dari tabel database -->
+                            <?php
+                            $index = 1; // Variabel untuk nomor urut
+                            /* Query untuk mengambil data dari tabel posts, users, dan categories */
+                            $query = "SELECT posts.*, users.name as user_name, categories.category_name FROM posts 
+                                      INNER JOIN users ON posts.user_id = users.user_id 
+                                      LEFT JOIN categories ON posts.category_id = categories.category_id 
+                                      WHERE posts.user_id = $userId";
+                            // Eksekusi query
+                            $exec = mysqli_query($conn, $query);
 
-<!-- menampilkan data dari tabel database -->
-<?php
-$index = 1; // variebel untuk nomor urut
-/* query untuk mengambil data dari tabel
-posts, users, dan categories */
-$query = "SELECT posts.*, users.name as user_name,
-categories.category_name FROM posts 
-INNER JOIN users ON posts.user_id = users.users_id
-LEFT JOIN categories ON posts.category_id = categories.category_id
-WHERE posts.user_id = $userId";
-// eksekusi query
-$exec = mysqli_query($conn, $query);
-
-// perulangan untuk menampilkan setiap baris hasil query
-while ($post = mysqli_fetch_assoc($exec)) :
-?>
-<tr>
-    <td><?= $index++; ?></td>
-    <td><? $post['post_title']; ?></td>
-    <td><? $post['user_name']; ?></td>
-    <td><? $post['category_name']; ?></td>
-    <td>
-      <div class="dropdown">
-        <!-- tombol dropdown untuk pilihan -->
-         <button type="button" class="btn p-0 dropdown-tonggle hide-arrow" data-bs-tonggle="dropdown">
-          <i class="bx bx-dots-vertical-rounded"></i>
-         </button>
-         <!-- menu dropdown -->
-          <div class="dropdown-menu">
-            <!-- pilihan edit -->
-             <a href="edit_post.php?post_id=<?= $post['id_post']; ?>"
-             class="dropdown-item">
-               <i class="bx bx-edit-alt me-2"></i> Edit
-             </a>
-            <!-- pilihan -->
-             <a href="#" class="dropdown-item" data-bs-tonggle="modal"
-             data-bs-target="#deletePost_<?= $post['id_post']; ?>">
-             <i class="bx bx-trash me-2"></i> Delete
-            </a>
-          </div>
-      </div>
-    </td>
-  </tr>
-    <!-- modal untuk hapus konten blog -->
-    <div class="modal fade" id="deletePost_<?= $post['id_post']; ?>" tabindex="-1" aria-hidden="true">
-    <div class="modal_dialog" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title">Hapus Post?</h5>
-          <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                            // Perulangan untuk menampilkan setiap baris hasil query
+                            while ($post = mysqli_fetch_assoc($exec)) :
+                            ?>
+                            <tr>
+                                <td><?= $index++; ?></td>
+                                <td><?= $post['post_title']; ?></td>
+                                <td><?= $post['user_name']; ?></td>
+                                <td><?= $post['category_name']; ?></td>
+                                <td>
+                                    <div class="dropdown">
+                                        <!-- Tombol dropdown untuk Pilihan -->
+                                        <button type="button" class="btn p-0 dropdown-toggle hide-arrow" data-bs-toggle="dropdown">
+                                            <i class="bx bx-dots-vertical-rounded"></i>
+                                        </button>
+                                        <!-- Menu dropdown -->
+                                        <div class="dropdown-menu">
+                                            <!-- Pilihan Edit -->
+                                            <a href="edit_post.php?post_id=<?= $post['id_post']; ?>" class="dropdown-item">
+                                                <i class="bx bx-edit-alt me-2"></i> Edit
+                                            </a>
+                                            <!-- Pilihan Delete -->
+                                            <a href="#" class="dropdown-item" data-bs-toggle="modal" data-bs-target="#deletePost_<?= $post['id_post']; ?>">
+                                                <i class="bx bx-trash me-2"></i> Delete
+                                            </a>
+                                        </div>
+                                    </div>
+                                    <!-- Modal untuk Hapus Konten Blog -->
+                                    <div class="modal fade" id="deletePost_<?= $post['id_post']; ?>" tabindex="-1" aria-hidden="true">
+                                        <div class="modal-dialog" role="document">
+                                            <div class="modal-content">
+                                                <div class="modal-header">
+                                                    <h5 class="modal-title">Hapus Post?</h5>
+                                                    <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
+                                                </div>
+                                                <div class="modal-body">
+                                                    <form action="proses_post.php" method="POST">
+                                                        <p>Tindakan ini tidak bisa dibatalkan.</p>
+                                                        <input type="hidden" name="postID" value="<?= $post['id_post']; ?>">
+                                                        <div class="modal-footer">
+                                                            <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">Batal</button>
+                                                            <button type="submit" name="delete" class="btn btn-primary">Hapus</button>
+                                                        </div>
+                                                    </form>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </td>
+                            </tr>
+                            <?php endwhile; ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
-      <div class="modal-body">
-       <form action="proses_post.php" method="POST">
-        <div>
-          <div>
-            <p>Tindakan ini tidak bisa di batalkan</p>
-            <input type="hidden" name="postID" value="<?= $post['id_post']?>">
-          </div>
-          <div class="modal-footer">
-            <button type="button" class="btn btn-outline-secondary"
-            data-bs-dismiss="modal">Batal</button>
-            <button type="submit" name="delete" class="btn btn-primary"
-            >Hapus</button>
-          </div>
-        </form>
-      </div>
     </div>
-  </div>
-</div>
-<?php endwhile; ?>
-          </tbody>
-        </table>
-       </div>
-      </div>
-    </div>
-    <!-- akhir tabel dengan baris yang dapat di-hover -->
-  </div>
 </div>
 
-<?php 
-include (".includes/footer.php")
+<?php
+include (".includes/footer.php");
 ?>
