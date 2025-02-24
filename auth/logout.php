851470215df418efe9a2c22251e769199d95e4c9
jefrial-rpl <jeff711720@gmail.com>
2025-02-24 08:47:56 +0700

    pkpkpk

--- auth/logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start(); // memulai sesi
-session_unset(); // menghapus semua
-session_destroy(); // menghancurkan sesi sepenuhnya
-header('Location: login.php'); // arahkan pengguna ke halaman login
-exit(); //menghentikan eksekusi script
-?>
